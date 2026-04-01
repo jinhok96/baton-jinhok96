66170e3c1dd6eafe09cbf87d58aad0697d821e61
jinhok96 <jinhok96a@gmail.com>
2026-04-02 02:32:54 +0900

    Chore: eslint와 prettier 호환 패치, 린팅 npm 스크립트 추가

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,8 +1,10 @@
 import js from '@eslint/js';
+import prettier from 'eslint-config-prettier';
 import globals from 'globals';
 
 export default [
   js.configs.recommended,
+  prettier,
   {
     languageOptions: {
       globals: {
