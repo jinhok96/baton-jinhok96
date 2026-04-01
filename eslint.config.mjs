02140707ed7ad438b5d20a4e7d926f0b606db5da
jinhok96 <jinhok96a@gmail.com>
2026-04-02 02:10:40 +0900

    Chore: eslint, prettier 추가 및 설정, 워크플로우 deploy-dev와 deploy-main을 deploy-skin으로 통합

---
+++ eslint.config.mjs
@@ -0,0 +1,21 @@
+import js from '@eslint/js';
+import globals from 'globals';
+
+export default [
+  js.configs.recommended,
+  {
+    languageOptions: {
+      globals: {
+        ...globals.browser,
+        ...globals.jquery,
+      },
+    },
+    rules: {
+      'no-unused-vars': 'warn',
+      'no-console': 'off',
+    },
+  },
+  {
+    ignores: ['js/swiper-bundle.js', '**/*.html'],
+  },
+];
