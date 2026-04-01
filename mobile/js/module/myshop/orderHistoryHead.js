65e9f9fd5100ac27b51887fa773f5a26832c5bb3
jinhok96 <jinhok96a@gmail.com>
2026-04-01 22:51:32 +0900

    Chore: 스킨 경로를 skin2 (live-jinhok96)으로 변경하고 skin2 데이터 다운로드

--- mobile/js/module/myshop/orderHistoryHead.js
+++
@@ -1,6 +0,0 @@
-//기간 설정하기
-$('.xans-myshop-orderhistoryhead .eDataSet').on('click', function() {
-    $('#dataSearch').toggle();
-    $(this).siblings().removeClass('selected');
-    OrderHistory.set_period_mode('search');
-});
